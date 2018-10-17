b2fecf5eb8bd07050b2dbfc19d3bd0f681eea3d2
Justin Weiskittel <justinweiskittel@gmail.com>
2018-10-17 16:20:30 -0400

    fuck you again

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000'
         break
-    case 'jw-gtclient.herokuapp.com/':
-        APIURL = 'https://jw-gtserver.herokuapp.com/'
+    case 'jw-gtclient.herokuapp.com':
+        APIURL = 'https://jw-gtserver.herokuapp.com'
 }
 
 export default APIURL
